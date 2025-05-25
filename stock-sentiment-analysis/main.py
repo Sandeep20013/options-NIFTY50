1caca5dd01356480afa93cec2053d2018ad5f2ac
Sandeep20013 <sandeepm20013@gmail.com>
2025-05-25 21:03:16 +0100

    added postgres support

--- stock-sentiment-analysis/main.py
+++ stock-sentiment-analysis/main.py
@@ -4,8 +4,19 @@ from helpers.clean_data import IndianNewsDataCleaner
 from helpers.tokenizer_indian import tokenize_function
 from datasets import Dataset
 from sklearn.model_selection import train_test_split
-
-df = pd.read_csv("data/Indian_Financial_News.csv", index_col=0)
+from postgres_scripts.read_data import load_financial_news
+import os
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASS')
+host = os.getenv('DB_HOST')
+port = os.getenv('DB_PORT')
+database = os.getenv('DB_NAME')
+df = load_financial_news(database=database,
+                         host=host,
+                         password=password,
+                         port=port,
+                         user=user
+                         )
 cleaner = IndianNewsDataCleaner(df, country='India', label = 'Sentiment')
 df_clean = (
     cleaner
@@ -27,5 +38,6 @@ train_tokenized = train_dataset.map(tokenize_function, batched=True)
 val_tokenized = val_dataset.map(tokenize_function, batched=True)
 test_tokenized = test_dataset.map(tokenize_function, batched=True)
 
-print(train_tokenized[0])
+# print(train_tokenized[0])
 # Set format for pytorch
+
