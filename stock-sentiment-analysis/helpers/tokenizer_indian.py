e70aecf278b380f77b2b0ee58f358d315e30c4d3
Sandeep20013 <sandeepm20013@gmail.com>
2025-06-07 17:25:21 +0100

    changed tokenizer

--- stock-sentiment-analysis/helpers/tokenizer_indian.py
+++ stock-sentiment-analysis/helpers/tokenizer_indian.py
@@ -2,6 +2,5 @@ from transformers import BertTokenizer
 import re
 tokenizer = BertTokenizer.from_pretrained("yiyanghkust/finbert-tone")
 
-def tokenize_function(examples, X='Content'):
-    return tokenizer(examples[X], padding="max_length", truncation=True, max_length=128)
-
+def tokenize_function(text):
+    return tokenizer(text["text"], padding="max_length", truncation=True, max_length=128)
