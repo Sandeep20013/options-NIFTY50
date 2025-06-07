d066096e68fa704d1eb2d127d1d962b74eed5ee2
Sandeep20013 <sandeepm20013@gmail.com>
2025-06-07 17:25:10 +0100

    added filtering and cleaning

--- stock-sentiment-analysis/helpers/clean_data.py
+++ stock-sentiment-analysis/helpers/clean_data.py
@@ -1,10 +1,10 @@
 import re
 class IndianNewsDataCleaner:
-    def __init__(self, df, country = 'India', label = 'Sentiment', X = 'Content'):
-        self.df = df
+    def __init__(self, df, country='India', label='Sentiment', desc_col='Content'):
+        self.df = df.copy()
         self.country = country
         self.label = label
-        self.X = X
+        self.desc_col = desc_col
 
     def map_sentiment(self):
         self.df[self.label] = self.df[self.label].map({'Negative': 0, 'Neutral': 1, 'Positive': 2})
@@ -13,20 +13,29 @@ class IndianNewsDataCleaner:
     def add_country(self):
         self.df['country'] = self.country
         return self
-    
+
     def clean_text(self):
         def clean(t):
+            t = str(t)
+            t = re.sub(r'http\S+|www\S+|https\S+', '', t, flags=re.MULTILINE)
+            t = re.sub(r'\S+@\S+', '', t)
             t = t.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
             t = re.sub(r'\s+', ' ', t)
+            t = re.sub(r'[^\w\s,.!?-]', '', t)
+            t = t.lower()
             return t.strip()
-        
-        self.df[self.X] = self.df[self.X].astype(str).apply(clean)
+
+        self.df[self.desc_col] = self.df[self.desc_col].apply(clean)
         return self
-    
+
+
+
     def filter_data(self):
-        self.df = self.df[[self.X, self.label]]
+        self.df = self.df.drop_duplicates(subset=[self.desc_col, self.label])
+        self.df = self.df.dropna(subset=[self.desc_col, self.label])
+        self.df = self.df[self.df[self.desc_col].str.len() > 20]  # minimum length threshold
+        self.df = self.df[[self.desc_col, self.label]]
         return self
 
     def get_clean_data(self):
-        return self.df
-    
+        return self.df.rename(columns={self.desc_col: 'text'})
