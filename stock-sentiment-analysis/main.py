e642f857d9f9c8fe0f1f9a6a9ee66b264b5db1e4
Sandeep20013 <sandeepm20013@gmail.com>
2025-06-07 17:24:45 +0100

    torch check

--- stock-sentiment-analysis/main.py
+++ stock-sentiment-analysis/main.py
@@ -1,16 +1,18 @@
 from mlflow_scripts import mlflow_finbert
+import torch
 
 def main():
+    print(torch.cuda.is_available())
     params = {
         "learning_rate": 2e-5,
         "per_device_train_batch_size": 16,
         "per_device_eval_batch_size": 16,
-        "num_train_epochs": 4,
+        "num_train_epochs": 3,
         "weight_decay": 0.01,
         "model_name": "yiyanghkust/finbert-tone"
     }
 
-    mlflow_finbert(params)
+    mlflow_finbert.run_mlflow_experiment(params)
 
 if __name__ == "__main__":
     main()
