0faff46d170686ae91ac217645062459b4d35920
Sandeep20013 <sandeepm20013@gmail.com>
2025-06-17 16:52:26 +0100

    replaced placeholder exp name

--- stock-sentiment-analysis/mlflow_scripts/mlflow_finbert.py
+++ stock-sentiment-analysis/mlflow_scripts/mlflow_finbert.py
@@ -3,8 +3,8 @@ import mlflow.transformers
 from models.finbert import train_finbert
 from helpers.loaders.data_loader import get_tokenized_datasets  # You must implement this
 from metrics.eval_metrics import evaluate_finbert
-def run_mlflow_experiment(params):
-    experiment_name = "FinBERT_India_1"
+def run_mlflow_experiment(params, exp_name):
+    experiment_name = exp_name
     mlflow.set_experiment(experiment_name)
     train_dataset, val_dataset, test_dataset = get_tokenized_datasets()
     
@@ -26,8 +26,9 @@ def run_mlflow_experiment(params):
         test_metrics = evaluate_finbert(test_dataset, model_dir=params["model_name"])
         conf_path = test_metrics.pop("confusion_matrix_path")
         mlflow.log_metrics({f"test_{k}": v for k, v in test_metrics.items()})
-        # Log confusion matrix image
         mlflow.log_artifact(conf_path)
+        print("Logging model and tokenizer to MLflow...")
+        print(f"Model type: {type(model)}, Tokenizer type: {type(tokenizer)}")
         mlflow.transformers.log_model(
             transformers_model=model,
             artifact_path="finbert-india-model_1",
