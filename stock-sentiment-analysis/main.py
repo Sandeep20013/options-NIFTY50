4670ff1b3625a7c879a58e75908d170e6bcc6d2c
Sandeep20013 <sandeepm20013@gmail.com>
2025-06-17 16:13:28 +0100

    changed batch size

--- stock-sentiment-analysis/main.py
+++ stock-sentiment-analysis/main.py
@@ -5,14 +5,14 @@ def main():
     print(torch.cuda.is_available())
     params = {
         "learning_rate": 2e-5,
-        "per_device_train_batch_size": 16,
-        "per_device_eval_batch_size": 16,
-        "num_train_epochs": 3,
+        "per_device_train_batch_size": 32,
+        "per_device_eval_batch_size": 32,
+        "num_train_epochs": 5,
         "weight_decay": 0.01,
         "model_name": "yiyanghkust/finbert-tone"
     }
 
-    mlflow_finbert.run_mlflow_experiment(params)
+    mlflow_finbert.run_mlflow_experiment(params, "Finbert_test_2")
 
 if __name__ == "__main__":
     main()
